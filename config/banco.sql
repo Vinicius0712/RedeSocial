183f2f7e08522b2fd6878baeef7e62eff8ee21ce
vinicius <viniciuspereira0712@gmail.com>
2024-10-17 16:52:05 -0300

    implementando banco de dados no perfil

--- config/banco.sql
+++ config/banco.sql
@@ -7,7 +7,25 @@ CREATE TABLE usuarios (
     senha VARCHAR(250)
 );
 
-INSERT INTO usuarios (id,  usuario, senha) VALUES (1, 'Vini', '123');
+CREATE TABLE perfis (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    foto_perfil VARCHAR(255) DEFAULT 'default.jpg',
+    seguidores INT DEFAULT 0,
+    seguindo INT DEFAULT 0,
+    publicacoes INT DEFAULT 0,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+CREATE TABLE publicacoes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    imagem VARCHAR(255),
+    descricao TEXT,
+    data_publicacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
 
 
 create user 'Vinn'@'localhost' identified by 'Vinn'; -- cria o usuário para acessar ao banco de dados com o login fulano e senha 123
